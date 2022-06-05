c3b30211582d1570b2a59903b785b1c110052c24
FluSett <flusett@gmail.com>
2022-06-05 17:14:33 +0300

    create models for users

---
+++ lib/models/users/director_model.dart
@@ -0,0 +1,8 @@
+class DirectorModel {
+  String? firstName;
+  String? surname;
+  String? middleName;
+  String? school;
+
+  DirectorModel(this.firstName, this.surname, this.middleName, [this.school]);
+}
