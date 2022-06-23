d431182c6520f7eac45f0aa619e937a7cba33910
FluSett <flusett@gmail.com>
2022-06-23 16:34:04 +0300

    send request for teacher first auth

--- lib/database/firestore_requests_handler.dart
+++ lib/database/firestore_requests_handler.dart
@@ -10,6 +10,7 @@ class FirestoreRequestsHandler {
     String schoolId,
     String schoolName,
     String status,
+    String message,
   ) async {
     _requests.doc(uid).set({
       'uid': uid,
@@ -17,6 +18,25 @@ class FirestoreRequestsHandler {
       'schoolId': schoolId, //TODO: REWORK TO DYNAMIC DATA BY UID
       'schoolName': schoolName,
       'status': status,
+      'message': message
+    });
+  }
+
+  Future<void> sendTeacherSchoolRequest(
+    String uid,
+    String pib,
+    String schoolId,
+    String schoolName,
+    String status,
+    String message,
+  ) async {
+    _requests.doc(uid).set({
+      'uid': uid,
+      'pib': pib, //TODO: REWORK TO DYNAMIC DATA BY UID
+      'schoolId': schoolId, //TODO: REWORK TO DYNAMIC DATA BY UID
+      'schoolName': schoolName,
+      'status': status,
+      'message': message
     });
   }
 }
