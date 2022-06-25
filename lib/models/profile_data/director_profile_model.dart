9cf4fa4bd5d5f17fa2fd4896645652568f3ad085
FluSett <flusett@gmail.com>
2022-06-25 04:20:32 +0300

    user models for full data

---
+++ lib/models/profile_data/director_profile_model.dart
@@ -0,0 +1,12 @@
+class DirectorProfileModel {
+  String firstName;
+  String surname;
+  String middleName;
+  String role;
+  String school;
+  String email;
+  String id;
+
+  DirectorProfileModel(this.firstName, this.surname, this.middleName, this.role,
+      this.school, this.email, this.id);
+}
---
+++ lib/models/profile_data/teacher_profile_model.dart
@@ -0,0 +1,12 @@
+class TeacherProfileModel {
+  String firstName;
+  String surname;
+  String role;
+  String school;
+  List<String> subjects;
+  String email;
+  String id;
+
+  TeacherProfileModel(this.firstName, this.surname, this.role, this.school,
+      this.subjects, this.email, this.id);
+}
